62799816454abdc1e755997f0c70609eba719857
Rafael Soares <893971+rafasoares@users.noreply.github.com>
2022-05-06 13:42:02 +0200

    Update dot_config/zsh/aliases.zsh

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -1,7 +1,13 @@
-alias kcg="kcuc gapfish"
-alias kcgs="kcuc gapfish-staging"
 alias zrc="edit_settings ~/.zshrc"
 alias omz="edit_settings ~/.oh-my-zsh"
 alias pl10k="edit_settings ~/.p10k.zsh"
+
 alias bnfo="bundle info"
 alias b="bundle"
+
+# gcs is `git commit -S` by default, but I already sign all my commits
+# this is more useful :)
+alias gcs="git checkout staging"
+
+alias kcg="kcuc gapfish"
+alias kcgs="kcuc gapfish-staging"
