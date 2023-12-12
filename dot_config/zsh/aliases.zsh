5bdfa6529cff8084d4d6086467a3f9fbb0370058
Rafael Soares <893971+rafasoares@users.noreply.github.com>
2023-12-12 13:19:09 +0100

    Update .config/zsh/aliases.zsh

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -14,18 +14,18 @@ if command -v eza &> /dev/null; then
   alias ls='eza --icons --group-directories-first --git --color=always'
 
   # From https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/common-aliases
-# ls, the common ones I use a lot shortened for rapid fire usage
-alias l='ls -lFh'     #size,show type,human readable
-alias la='ls -laFh'   #long list,show almost all,show type,human readable
-alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
-alias lt='ls -lFh -stime'   #long list,sorted by date,show type,human readable
-alias ll='ls -l'      #long list
-alias ldot='ls -ld .*' #list dot files
-alias lS='ls -1Fh -ssize'   #single column,size,human readable
-alias lart='ls -1Fa -stime' #single column,all,recursive,sorted by date
-alias lrt='ls -1F'   #single column,recursive,show type
-alias lsr='ls -laRFh' #Recursive list of files and directories
-alias lsn='ls -1'     #A column contains name of files and directories
+  # adapted to use eza
+  alias l='ls -lFh'                         # size, show type, human readable
+  alias la='ls -laFh'                       # long list, show almost all, show type, human readable
+  alias lr='ls -RFh -stime'                 # sorted by date, recursive, show type, human readable
+  alias lt='ls -lFh -stime'                 # long list, sorted by date, show type, human readable
+  alias ll='ls -l'                          # long list
+  alias ldot='ls -ld .*'                    # list dot files
+  alias lS='ls -lFhr -ssize --total-size'   # long list, sort by size, human readable
+  alias lart='ls -1Fa -stime'               # 1 col, recursive, sorted by date, show almost all
+  alias lrt='ls -1F -stime'                 # 1 col, recursive, sorted by date
+  alias lsr='ls -laRFh'                     # Recursive list of files and directories
+  alias lsn='ls -1'                         # A column contains name of files and directories
 fi
 
 # gcs is `git commit -S` by default, but I already sign all my commits
