d6f9516b78f92ffac0c4847bb925cd1d194c0bdc
Rafael Soares <893971+rafasoares@users.noreply.github.com>
2023-12-12 12:41:35 +0100

    Update .config/zsh/aliases.zsh

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -5,13 +5,28 @@ alias pl10k="edit_settings ~/.p10k.zsh"
 alias bnfo="bundle info"
 alias b="bundle"
 
-alias cat='bat --style header --style rules --style snip --style changes --style header'
-alias eza='eza --icons --group-directories-first --git --color=always'
-alias l='eza -lh'
-alias ll='eza -lh'
-alias la='eza -lah'
-alias lt='eza -lTh'
-alias lta='eza -lTah'
+
+if command -v bat &> /dev/null; then
+  alias cat='bat --style header --style rules --style snip --style changes --style header'
+fi
+
+if command -v eza &> /dev/null; then
+  alias ls='eza --icons --group-directories-first --git --color=always'
+
+  alias l='ls -lFh'
+	alias lS='ls -1FSsh'
+	alias la='ls -lAFh'
+	alias lart='ls -1Fcart'
+	alias ldot='ls -ld .*'
+	alias ll='ls -l'
+	alias lr='ls -tRFh'
+	alias lrt='ls -1Fcrt'
+	alias ls='ls -G'
+	alias lsa='ls -lah'
+	alias lsn='ls -1'
+	alias lsr='ls -lARFh'
+	alias lt='ls -ltFh'
+fi
 
 # gcs is `git commit -S` by default, but I already sign all my commits
 # this is more useful :)
